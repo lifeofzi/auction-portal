fd078f2b294259859ba0c62fd7d43a5e9a64ee9d
kr123 <krishnanr1997@gmail.com>
2017-10-30 11:43:59 +0000

    1. Implemented Login Page : partially 2. Database Created

--- auction.py
+++ auction.py
@@ -1,17 +1,60 @@
-from flask import Flask,request
-app = Flask(__name__)
+from flask import Flask, request, render_template , session, redirect , url_for
+from flaskext.mysql import MySQL
 import os
+app = Flask(__name__)
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'AuctionPortal'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+app.config['SECRET_KEY'] = '12345'
+
+mysql = MySQL()
+mysql.init_app(app)
+
 
 @app.route('/')
 def index():
-    
-    return 'Index Page'
+    return render_template('index.html')
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+@app.route('/home')
+def home():
+    if session.get('logged_in',False)== True :
+        return render_template('home.html',username=session['user_ID'])
+    else: 
+        return redirect(url_for('login'))
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email',' ')
+        password = request.form.get('password',' ')
+        cursor = mysql.connect().cursor()
+        cursor.execute("SELECT * from users where email='" + email + "' and password='" + password + "'") #Correct here
+        data = cursor.fetchone()
+        if data is None:
+            return "Username or Password is wrong"
+        else:
+            print data
+            session['logged_in'] = True
+            session['user_ID'] = data[0]
+            #print session
+            print  "Logged in successfully"
+            return redirect(url_for('home'))
+    else:
+        return render_template('login.html')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        print request.form.get('user','assd')
+        return 'Hello'
+    else:
+        return render_template('signup.html')
 
 
 if __name__ == '__main__': # Start the flask server when running this script.
-   app.run('0.0.0.0',int(os.environ.get('PORT',8080)),debug = True)
+   app.run('0.0.0.0',int(os.environ.get('PORT',8082)) , debug = True)
--- schema.sql
+++ schema.sql
@@ -1,50 +1,168 @@
-drop table users;
-create table users (
-    id int primary key,
-    name varchar(80),
-    avatar int,
-    email varchar(80),
-    password varchar(80),
-    dateofjoin date,
-    account_balance int
-);
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 30, 2017 at 10:46 AM
+-- Server version: 5.5.57-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.22
 
-drop table items;
-create table items(
-    id varchar(80),
-    auction_id varchar(80),
-    name varchar(80),
-    description varchar(1000),
-    picture1 varchar(80),
-    picture2 varchar(80),
-    picture3 varchar(80),
-    start_price int,
-    increment_price int,
-    category int
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-drop table auctions;
-create table auctions(
-    id varchar(80),
-    name varchar(80),
-    admin_id varchar(80),
-    start_time time,
-    end_time time,
-    entry_fee int
-);
 
-drop table bids;
-create table bids(
-    id varchar(80),
-    item_id varchar(80),
-    bid_amount int,
-    bid_time time,
-    user_id varchar(80),
-    auction_id varchar(80)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
-drop table categories;
-create table categories(
-    id varchar(80),
-    name varchar(80)
-);
+--
+-- Database: `AuctionPortal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auctions`
+--
+
+CREATE TABLE IF NOT EXISTS `auctions` (
+  `auction_id` varchar(80) NOT NULL DEFAULT '',
+  `name` varchar(80) DEFAULT NULL,
+  `admin_id` int(11) DEFAULT NULL,
+  `start_time` time DEFAULT NULL,
+  `end_time` time DEFAULT NULL,
+  `entry_fee` int(11) DEFAULT NULL,
+  PRIMARY KEY (`auction_id`),
+  KEY `admin_id` (`admin_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bids`
+--
+
+CREATE TABLE IF NOT EXISTS `bids` (
+  `bid_id` varchar(80) NOT NULL DEFAULT '',
+  `item_id` varchar(80) DEFAULT NULL,
+  `bid_amount` float DEFAULT NULL,
+  `bid_time` time DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `auction_id` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`bid_id`),
+  KEY `item_id` (`item_id`),
+  KEY `user_id` (`user_id`),
+  KEY `auction_id` (`auction_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `category_id` varchar(80) NOT NULL DEFAULT '',
+  `name` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE IF NOT EXISTS `items` (
+  `iterm_id` varchar(80) NOT NULL DEFAULT '',
+  `auction_id` varchar(80) DEFAULT NULL,
+  `name` varchar(80) DEFAULT NULL,
+  `description` varchar(1000) DEFAULT NULL,
+  `picture1` varchar(80) DEFAULT NULL,
+  `picture2` varchar(80) DEFAULT NULL,
+  `picture3` varchar(80) DEFAULT NULL,
+  `start_price` int(11) DEFAULT NULL,
+  `increment_price` int(11) DEFAULT NULL,
+  `category` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`iterm_id`),
+  KEY `auction_id` (`auction_id`),
+  KEY `category` (`category`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchases`
+--
+
+CREATE TABLE IF NOT EXISTS `purchases` (
+  `purchase_id` varchar(80) NOT NULL,
+  `price` float NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `item_id` varchar(80) NOT NULL,
+  PRIMARY KEY (`purchase_id`),
+  KEY `user_id` (`user_id`),
+  KEY `item_id` (`item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(80) DEFAULT NULL,
+  `avatar` int(11) DEFAULT NULL,
+  `email` varchar(80) DEFAULT NULL,
+  `password` varchar(80) DEFAULT NULL,
+  `dateofjoin` date DEFAULT NULL,
+  `account_balance` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `avatar`, `email`, `password`, `dateofjoin`, `account_balance`) VALUES
+(0, 'Zaman Ishtiyaq', NULL, 'zi10@iitbbs.ac.in', 'password', '2017-10-11', 100);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `auctions`
+--
+ALTER TABLE `auctions`
+  ADD CONSTRAINT `auction-user-id` FOREIGN KEY (`admin_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `bids`
+--
+ALTER TABLE `bids`
+  ADD CONSTRAINT `bid-auction-fk` FOREIGN KEY (`auction_id`) REFERENCES `auctions` (`auction_id`),
+  ADD CONSTRAINT `bid-item-fk` FOREIGN KEY (`item_id`) REFERENCES `items` (`iterm_id`),
+  ADD CONSTRAINT `bid-user-fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `items`
+--
+ALTER TABLE `items`
+  ADD CONSTRAINT `category-item-od` FOREIGN KEY (`category`) REFERENCES `categories` (`category_id`),
+  ADD CONSTRAINT `auction-item-id` FOREIGN KEY (`auction_id`) REFERENCES `auctions` (`auction_id`);
+
+--
+-- Constraints for table `purchases`
+--
+ALTER TABLE `purchases`
+  ADD CONSTRAINT `purchases-item-fk` FOREIGN KEY (`item_id`) REFERENCES `items` (`iterm_id`),
+  ADD CONSTRAINT `purchases-user-fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
