e64ed9a4356e4a288c47956c2a299cfb39c0e76e
kr123 <krishnanr1997@gmail.com>
2017-11-13 11:38:50 +0000

    Schema Upload

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 30, 2017 at 10:46 AM
+-- Generation Time: Nov 13, 2017 at 11:36 AM
 -- Server version: 5.5.57-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.22
 
@@ -19,6 +19,56 @@ SET time_zone = "+00:00";
 --
 -- Database: `AuctionPortal`
 --
+CREATE DATABASE IF NOT EXISTS `AuctionPortal` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `AuctionPortal`;
+
+DELIMITER $$
+--
+-- Procedures
+--
+DROP PROCEDURE IF EXISTS `finish_auction`$$
+CREATE DEFINER=`k1997r`@`%` PROCEDURE `finish_auction`()
+    NO SQL
+begin
+declare finished integer default 0;
+declare aid integer;
+declare bd_amt float;
+declare uid integer;
+declare cur1 cursor for select auction_id from auctions where end_time < NOW() and status=FALSE;
+declare continue handler for not found set finished=1;
+
+open cur1;
+
+get_auctions:LOOP
+	SET finished = 0 ;
+	FETCH cur1 into aid;
+	select finished,aid;
+	if finished = 1 then leave get_auctions; end if;
+	
+	select user_id,bid_amount
+	into uid,bd_amt
+	from bids 
+	where auction_id=aid and bid_amount=(select max(bid_amount) from bids where auction_id=aid);
+	
+    	select bd_amt,uid;  
+		if bd_amt is not null then
+	
+		update users join bids on bids.user_id=users.id
+		set account_balance=account_balance+bids.bid_amount
+		where bids.auction_id=aid and users.id != uid;
+
+
+		insert into purchases(auction_id,price,user_id) values(aid,bd_amt,uid);
+		end if;
+	
+		update auctions set status=TRUE where auction_id=aid;
+end loop get_auctions;
+
+close cur1;
+
+END$$
+
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -26,16 +76,46 @@ SET time_zone = "+00:00";
 -- Table structure for table `auctions`
 --
 
+DROP TABLE IF EXISTS `auctions`;
 CREATE TABLE IF NOT EXISTS `auctions` (
-  `auction_id` varchar(80) NOT NULL DEFAULT '',
   `name` varchar(80) DEFAULT NULL,
+  `description` varchar(1000) DEFAULT NULL,
   `admin_id` int(11) DEFAULT NULL,
-  `start_time` time DEFAULT NULL,
-  `end_time` time DEFAULT NULL,
-  `entry_fee` int(11) DEFAULT NULL,
+  `start_time` datetime DEFAULT NULL,
+  `end_time` datetime DEFAULT NULL,
+  `start_price` float NOT NULL DEFAULT '0',
+  `increment_price` float NOT NULL DEFAULT '0',
+  `picture1` varchar(800) NOT NULL,
+  `picture2` varchar(800) NOT NULL,
+  `picture3` varchar(800) NOT NULL,
+  `category_id` int(8) NOT NULL,
+  `auction_id` int(8) NOT NULL AUTO_INCREMENT,
+  `status` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`auction_id`),
-  KEY `admin_id` (`admin_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  KEY `admin_id` (`admin_id`),
+  KEY `category_id` (`category_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=47 ;
+
+--
+-- Triggers `auctions`
+--
+DROP TRIGGER IF EXISTS `trig_init_price_check`;
+DELIMITER //
+CREATE TRIGGER `trig_init_price_check` BEFORE INSERT ON `auctions`
+ FOR EACH ROW BEGIN 
+    IF NEW.increment_price <0
+	THEN 
+	SET NEW.increment_price =0;
+	END IF ;
+
+	IF NEW.start_price < 0
+	THEN 
+	SET NEW.start_price =0;
+	END IF ;
+
+    END
+//
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -43,18 +123,50 @@ CREATE TABLE IF NOT EXISTS `auctions` (
 -- Table structure for table `bids`
 --
 
+DROP TABLE IF EXISTS `bids`;
 CREATE TABLE IF NOT EXISTS `bids` (
-  `bid_id` varchar(80) NOT NULL DEFAULT '',
-  `item_id` varchar(80) DEFAULT NULL,
+  `bid_id` int(10) NOT NULL AUTO_INCREMENT,
   `bid_amount` float DEFAULT NULL,
-  `bid_time` time DEFAULT NULL,
+  `bid_time` datetime DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
-  `auction_id` varchar(80) DEFAULT NULL,
+  `auction_id` int(8) DEFAULT NULL,
   PRIMARY KEY (`bid_id`),
-  KEY `item_id` (`item_id`),
   KEY `user_id` (`user_id`),
   KEY `auction_id` (`auction_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=68 ;
+
+--
+-- Triggers `bids`
+--
+DROP TRIGGER IF EXISTS `update_balance`;
+DELIMITER //
+CREATE TRIGGER `update_balance` BEFORE INSERT ON `bids`
+ FOR EACH ROW BEGIN
+		SET @bal := (SELECT account_balance FROM users WHERE id=NEW.user_id);
+		IF @bal < NEW.bid_amount
+		THEN signal sqlstate '45000';
+		END IF;
+		IF @bal >= NEW.bid_amount
+		THEN UPDATE users SET account_balance=account_balance-NEW.bid_amount WHERE id=NEW.user_id;
+		END IF;
+	END
+//
+DELIMITER ;
+DROP TRIGGER IF EXISTS `update_balance_onupdate`;
+DELIMITER //
+CREATE TRIGGER `update_balance_onupdate` BEFORE UPDATE ON `bids`
+ FOR EACH ROW BEGIN
+		SET @bal := (SELECT account_balance FROM users WHERE id=NEW.user_id);
+		SET @prev_bid := (SELECT bid_amount FROM bids WHERE user_id=NEW.user_id AND auction_id=NEW.auction_id);
+		IF @bal < NEW.bid_amount
+		THEN signal sqlstate '45000';
+		END IF;
+		IF @bal >= NEW.bid_amount
+		THEN UPDATE users SET account_balance=account_balance-NEW.bid_amount+@prev_bid WHERE id=NEW.user_id;
+		END IF;
+	END
+//
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -62,33 +174,12 @@ CREATE TABLE IF NOT EXISTS `bids` (
 -- Table structure for table `categories`
 --
 
+DROP TABLE IF EXISTS `categories`;
 CREATE TABLE IF NOT EXISTS `categories` (
-  `category_id` varchar(80) NOT NULL DEFAULT '',
+  `category_id` int(8) NOT NULL AUTO_INCREMENT,
   `name` varchar(80) DEFAULT NULL,
   PRIMARY KEY (`category_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `items`
---
-
-CREATE TABLE IF NOT EXISTS `items` (
-  `iterm_id` varchar(80) NOT NULL DEFAULT '',
-  `auction_id` varchar(80) DEFAULT NULL,
-  `name` varchar(80) DEFAULT NULL,
-  `description` varchar(1000) DEFAULT NULL,
-  `picture1` varchar(80) DEFAULT NULL,
-  `picture2` varchar(80) DEFAULT NULL,
-  `picture3` varchar(80) DEFAULT NULL,
-  `start_price` int(11) DEFAULT NULL,
-  `increment_price` int(11) DEFAULT NULL,
-  `category` varchar(80) DEFAULT NULL,
-  PRIMARY KEY (`iterm_id`),
-  KEY `auction_id` (`auction_id`),
-  KEY `category` (`category`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
 
 -- --------------------------------------------------------
 
@@ -96,15 +187,16 @@ CREATE TABLE IF NOT EXISTS `items` (
 -- Table structure for table `purchases`
 --
 
+DROP TABLE IF EXISTS `purchases`;
 CREATE TABLE IF NOT EXISTS `purchases` (
-  `purchase_id` varchar(80) NOT NULL,
+  `purchase_id` int(80) NOT NULL AUTO_INCREMENT,
   `price` float NOT NULL,
   `user_id` int(11) NOT NULL,
-  `item_id` varchar(80) NOT NULL,
+  `auction_id` int(8) NOT NULL,
   PRIMARY KEY (`purchase_id`),
   KEY `user_id` (`user_id`),
-  KEY `item_id` (`item_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  KEY `item_id` (`auction_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
 
 -- --------------------------------------------------------
 
@@ -112,24 +204,18 @@ CREATE TABLE IF NOT EXISTS `purchases` (
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(80) DEFAULT NULL,
   `avatar` int(11) DEFAULT NULL,
   `email` varchar(80) DEFAULT NULL,
   `password` varchar(80) DEFAULT NULL,
   `dateofjoin` date DEFAULT NULL,
-  `account_balance` int(11) DEFAULT NULL,
+  `account_balance` int(11) DEFAULT '1000',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `name`, `avatar`, `email`, `password`, `dateofjoin`, `account_balance`) VALUES
-(0, 'Zaman Ishtiyaq', NULL, 'zi10@iitbbs.ac.in', 'password', '2017-10-11', 100);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1023 ;
 
 --
 -- Constraints for dumped tables
@@ -139,29 +225,31 @@ INSERT INTO `users` (`id`, `name`, `avatar`, `email`, `password`, `dateofjoin`, 
 -- Constraints for table `auctions`
 --
 ALTER TABLE `auctions`
-  ADD CONSTRAINT `auction-user-id` FOREIGN KEY (`admin_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `auction-user-id` FOREIGN KEY (`admin_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `auctions_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`);
 
 --
 -- Constraints for table `bids`
 --
 ALTER TABLE `bids`
-  ADD CONSTRAINT `bid-auction-fk` FOREIGN KEY (`auction_id`) REFERENCES `auctions` (`auction_id`),
-  ADD CONSTRAINT `bid-item-fk` FOREIGN KEY (`item_id`) REFERENCES `items` (`iterm_id`),
-  ADD CONSTRAINT `bid-user-fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
---
--- Constraints for table `items`
---
-ALTER TABLE `items`
-  ADD CONSTRAINT `category-item-od` FOREIGN KEY (`category`) REFERENCES `categories` (`category_id`),
-  ADD CONSTRAINT `auction-item-id` FOREIGN KEY (`auction_id`) REFERENCES `auctions` (`auction_id`);
+  ADD CONSTRAINT `bid-user-fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `bids_ibfk_1` FOREIGN KEY (`auction_id`) REFERENCES `auctions` (`auction_id`);
 
 --
 -- Constraints for table `purchases`
 --
 ALTER TABLE `purchases`
-  ADD CONSTRAINT `purchases-item-fk` FOREIGN KEY (`item_id`) REFERENCES `items` (`iterm_id`),
-  ADD CONSTRAINT `purchases-user-fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `purchases-user-fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `purchases_ibfk_1` FOREIGN KEY (`auction_id`) REFERENCES `auctions` (`auction_id`);
+
+DELIMITER $$
+--
+-- Events
+--
+DROP EVENT `finish_auctions`$$
+CREATE DEFINER=`k1997r`@`%` EVENT `finish_auctions` ON SCHEDULE EVERY 1 MINUTE STARTS '2017-11-13 05:20:44' ON COMPLETION NOT PRESERVE ENABLE DO call finish_auction()$$
+
+DELIMITER ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
